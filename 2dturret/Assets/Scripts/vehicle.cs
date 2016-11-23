b11c88acde734b6be646aa6f89ad26fe40570d05
jrs9025 <jrs9025@rit.edu>
2016-11-23 00:06:03 -0500

    Stupid huge commit for backup

---
+++ 2dturret/Assets/Scripts/vehicle.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vehicle : MonoBehaviour {
+	//Defines
+	private Vector3 vehiclePos; // position of vehicle
+	public float speed= 1f; // speed of vehicle
+	private Vector3 velocity;
+	private Vector3 direction;
+	private Quaternion angle; 
+	private float zRotation;
+	private Vector3 accelVector;
+	private float maxSpeed= 0.1f;
+	public float accelRate=0.1f;
+	// Use this for initialization
+	void Start () {
+		vehiclePos = new Vector3 (0, 0, 0);
+		velocity = new Vector3 (0, 0, 0);
+		direction = new Vector3 (0, 1, 0);
+		zRotation = 0;
+		accelVector = new Vector3 (0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//add speed to both directions
+		//vehiclePos.x += speed*Time.deltaTime;
+		//vehiclePos.y += speed*Time.deltaTime;
+		Drive ();
+		RotateDirection ();
+	}
+
+	void RotateDirection(){
+		if(Input.GetKey(KeyCode.J)){
+			zRotation+=3;
+			angle= Quaternion.Euler(0,0,3);
+			direction = angle * direction;
+
+		}
+		if(Input.GetKey(KeyCode.L)){
+			angle= Quaternion.Euler(0,0,-3);
+			direction = angle * direction;
+			zRotation-=3;
+		}
+
+	}
+
+	void Drive(){
+		if (Input.GetKey (KeyCode.I)) {
+			accelRate+=0.1f;
+		}
+		if (!Input.GetKey (KeyCode.I) && accelRate>=0.05f) {
+			accelRate= accelRate*0.9f;
+
+		}
+		if (!Input.GetKey (KeyCode.I) && accelRate < 0.05f){
+			accelRate=0;
+		}
+		velocity = accelRate * direction* Time.deltaTime;
+		//velocity += accelVector;
+		velocity = Vector3.ClampMagnitude (velocity, maxSpeed);
+		vehiclePos += velocity;
+		WrapVehicle ();
+		SetTransform ();
+	}
+
+	void SetTransform(){
+		// sets the new position
+		transform.position = vehiclePos;
+		transform.rotation = Quaternion.Euler(0,0, zRotation);
+	}
+
+	void WrapVehicle(){
+		float x = transform.position.x;
+		float y = transform.position.y;
+		Vector3 wrapPos = vehiclePos;
+		if (transform.position.x >= 10.0f ) {
+			wrapPos.x= (-9.9f);
+		}
+		if (transform.position.x <= -10.0f ) {
+			wrapPos.x=9.9f;
+		}
+		if (transform.position.y >= 5.0f) {
+			wrapPos.y= (-4.9f);
+		}
+		if (transform.position.y <= -5.0f) {
+			wrapPos.y= 4.9f;
+		}
+		vehiclePos = wrapPos;
+	}
+
+	
+}
---
+++ 2dturret/Sellers_Standalone/Sellers_Collision/collision.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collision : MonoBehaviour {
+	public GameObject[] sprites;
+	private bool abOrBc= true; // True is AABB False is BoundingCircle defaults to AABB
+	private bool collide= true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ToggleType ();
+		//axis aligned bounding box
+		if (abOrBc) {
+			collide= AABB (sprites [0], sprites [1]);
+		//bounding circles
+		} else {
+			collide= BoudingCircle(sprites[0], sprites[1]);
+		}
+		ChangeColor (sprites [0], sprites [1]);
+	}
+
+	bool AABB(GameObject colA, GameObject colB){
+		SpriteRenderer a = colA.GetComponent<SpriteRenderer> ();
+		SpriteRenderer b = colB.GetComponent<SpriteRenderer> ();
+		//Top of a is in Bottom of b
+		if ((a.bounds.max.y > b.bounds.min.y)) {
+			//Right of a is in the Left of b
+			if ((a.bounds.max.x > b.bounds.min.x)&&!(a.bounds.max.x > b.bounds.max.x)) {
+				return true;
+			}
+			if ((a.bounds.min.x < b.bounds.max.x)&&!(a.bounds.min.x < b.bounds.min.x)) {
+				return true;
+			}
+
+		//Bottom of a is in Top of b
+		} else if ((a.bounds.min.y < b.bounds.max.y)) {
+			//Right of a is in the Left of b and the Right of a is not outside the Right of b return true
+			if ((a.bounds.max.x > b.bounds.min.x)&&!(a.bounds.max.x > b.bounds.max.x)) {
+				return true;
+			}
+			//Left of a is in the Right of b and the Left of a is not outside the Left of b retrun true
+			if ((a.bounds.min.x < b.bounds.max.x)&&!(a.bounds.min.x < b.bounds.min.x)) {
+				return true;
+			}
+		} 
+		//otherwise return false
+		return false;
+	}
+
+	bool BoudingCircle( GameObject colA, GameObject colB){
+		SpriteRenderer a = colA.GetComponent<SpriteRenderer> ();
+		SpriteRenderer b = colB.GetComponent<SpriteRenderer> ();
+		//Distance from the center x and y
+		float distX= a.bounds.center.x - b.bounds.center.x;
+		float distY= a.bounds.center.y - b.bounds.center.y;
+		//Square distance for faster calc
+		float squareDist = (distX * distX) + (distY * distY);
+		//"radius" from center to outside
+		float r1 = a.bounds.max.x - a.bounds.center.x;
+		float r2 = b.bounds.max.x - b.bounds.center.x;
+		//if the square distance is greater than the radius square return true
+		if(squareDist<= (r1+r2)*(r1+r2)){
+			return true;
+		}
+		//otherwise return false
+		return false;
+	}
+
+	void ToggleType(){
+		//if you click 1 the collide goes to axis alligned
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			abOrBc= true;
+		}
+		//if you click 2 the collide goes to bounding circle
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			abOrBc= false;
+		}
+	}
+	void ChangeColor(GameObject colA, GameObject colB){
+		//if colliding change to red
+		if (collide) {
+			colA.GetComponent<SpriteRenderer> ().color = Color.red;
+			colB.GetComponent<SpriteRenderer> ().color = Color.red;
+		}
+		//otherwise set the collors to default
+		else {
+			colA.GetComponent<SpriteRenderer> ().color = Color.white;
+			colB.GetComponent<SpriteRenderer> ().color = Color.white;
+		}
+	}
+}
